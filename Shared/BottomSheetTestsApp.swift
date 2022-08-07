f68ccd37327568b5fde7c19f42660d07d4fc8014
Lucas Zischka <63545066+lucaszischka@users.noreply.github.com>
2022-08-07 03:33:46 +0200

    Initial Commit

---
+++ Shared/BottomSheetTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheetTestsApp.swift
+//
+//  Created by Lucas Zischka.
+//  Copyright © 2022 Lucas Zischka. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheetTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//
+//  Created by Lucas Zischka.
+//  Copyright © 2022 Lucas Zischka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
